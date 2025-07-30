005574b5008234862b299e69b0858621e6f5fce4
Mr Philips <ddoudou7@yahoo.fr>
2025-07-30 09:54:47 +0000

    8. Complex types - functions

---
+++ python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Module for make_multiplier function."""
+
+from typing import Callable
+
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """Returns a function that multiplies a float by multiplier."""
+    return lambda x: x * multiplier
