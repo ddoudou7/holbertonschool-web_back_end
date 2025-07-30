60c6bb414e70dee287450a8e2467248bcee46a46
Mr Philips <ddoudou7@yahoo.fr>
2025-07-30 23:53:34 +0000

    0. Async Generator: fix docstring wrapping

--- python_async_function/0-async_generator.py
+++ python_async_function/0-async_generator.py
@@ -7,9 +7,6 @@ from typing import AsyncGenerator
 
 
 async def async_generator() -> AsyncGenerator[float, None]:
-    """
-    Yield a random float between 0 and 10, 10 times, with 1 second wait.
-    """
-    for _ in range(10):
+    """Yield a random float between 0 and 10, 10 times, with 1 second wait."""
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
