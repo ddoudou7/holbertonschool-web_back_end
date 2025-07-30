7465709847df29101b57e3a546dfa7446faa74a2
Mr Philips <ddoudou7@yahoo.fr>
2025-07-30 09:15:19 +0000

    Fix pycodestyle for 0-add.py

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
-"""This module provides a single function `add` which takes two floats and returns their sum as a float."""
+"""This module provides a single function `add`
+which takes two floats and returns their sum as a float."""
+
 
 def add(a: float, b: float) -> float:
     """Return the sum of two float values."""
