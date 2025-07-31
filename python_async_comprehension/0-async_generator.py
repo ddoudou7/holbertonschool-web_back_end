1e7ee0b7c0e6c26f00d9351878523cce7e2bd55d
Mr Philips <ddoudou7@yahoo.fr>
2025-07-31 01:13:37 +0000

    0. Async Generator

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
+"""Async Generator Task 0"""
+
 import asyncio
 import random
 from typing import AsyncGenerator
+
 
 async def async_generator() -> AsyncGenerator[float, None]:
     for _ in range(10):
