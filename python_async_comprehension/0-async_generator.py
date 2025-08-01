c3417b7b85470e4143f53c06dc2b18f01fddf512
Mr Philips <ddoudou7@yahoo.fr>
2025-08-01 13:24:54 +0000

    fix: pep8 compliance

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -1,14 +1,9 @@
 #!/usr/bin/env python3
-"""Async generator – yields 10 random floats"""
-
 import asyncio
 import random
-from typing import Generator
-from typing import Generator
 
 
-async def async_generator() -> Generator[float, None, None]:
-    """Yield 10 random floats between 0 and 10, one every second"""
+async def async_generator():
     for _ in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
