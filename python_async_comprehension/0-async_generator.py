9fb6d7922e37e6ad31c6c42b8393cb43c76084da
Mr Philips <ddoudou7@yahoo.fr>
2025-08-01 13:19:47 +0000

    0. Async Generator — correct Generator type

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -3,10 +3,11 @@
 
 import asyncio
 import random
-from typing import AsyncGenerator
+from typing import Generator
+from typing import Generator
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
+async def async_generator() -> Generator[float, None, None]:
     """Yield 10 random floats between 0 and 10, one every second"""
     for _ in range(10):
         await asyncio.sleep(1)
--- python_async_comprehension/0-main.py
+++ python_async_comprehension/0-main.py
@@ -1,8 +1,14 @@
 #!/usr/bin/env python3
+
 import asyncio
 
-wait_random = __import__('0-basic_async_syntax').wait_random
 
-print(asyncio.run(wait_random()))
-print(asyncio.run(wait_random()))
-print(asyncio.run(wait_random()))
+async_generator = __import__('0-async_generator').async_generator
+
+async def print_yielded_values():
+    result = []
+    async for i in async_generator():
+        result.append(i)
+    print(result)
+
+asyncio.run(print_yielded_values())
