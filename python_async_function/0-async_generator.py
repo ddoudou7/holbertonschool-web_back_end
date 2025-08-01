43f39c3bc9da1e3b170a024624d3183bc4f62004
Mr Philips <ddoudou7@yahoo.fr>
2025-08-01 13:49:06 +0000

    task0: move 0-async_generator.py to python_async_function/

---
+++ python_async_function/0-async_generator.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""Async generator - yields 10 random floats"""
+
+import asyncio
+import random
+from typing import AsyncGenerator
+
+
+async def async_generator() -> AsyncGenerator[float]:
+    """Yield 10 random floats between 0 and 10, one every second"""
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
