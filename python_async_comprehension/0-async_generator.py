367843da63d5eba8e49d844c8b6cbd7e59d3d3a7
Mr Philips <ddoudou7@yahoo.fr>
2025-08-02 00:53:13 +0000

    Add 0-async_generator.py with working async coroutine

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
-"""Async generator – Task 0"""
+"""Async generator coroutine"""
 
 import asyncio
 import random
+from typing import AsyncGenerator
 
-
-async def async_generator():
-    """Yield 10 random floats between 0 & 10, 1 s apart."""
+async def async_generator() -> AsyncGenerator[float, None]:
+    """Yield a random float between 0 and 10 every second, 10 times"""
     for _ in range(10):
         await asyncio.sleep(1)
-        yield random.random() * 10
+        yield random.uniform(0, 10)
--- python_async_comprehension/0-main.py
+++ python_async_comprehension/0-main.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 
 import asyncio
-
 async_generator = __import__('0-async_generator').async_generator
 
 async def print_yielded_values():
