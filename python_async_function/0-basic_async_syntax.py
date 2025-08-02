7c6102d85631c07ff638f21df80cafd2de6d8c2c
Mr Philips <ddoudou7@yahoo.fr>
2025-08-02 01:49:21 +0000

    Task 0: add wait_random coroutine (async basics)

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -1,11 +1,16 @@
 #!/usr/bin/env python3
-"""Module that defines an asynchronous coroutine to wait for a random delay"""
+"""Coroutine that waits a random delay and returns it."""
 
 import asyncio
 import random
+from typing import Union
 
-async def wait_random(max_delay: int = 10) -> float:
-    """Waits for a random delay between 0 and max_delay seconds."""
-    delay = random.uniform(0, max_delay)
+
+async def wait_random(max_delay: int = 10) -> Union[int, float]:
+    """
+    Wait for a random delay between 0 and max_delay seconds
+    and return the actual delay value.
+    """
+    delay: float = random.uniform(0, max_delay)
     await asyncio.sleep(delay)
     return delay
