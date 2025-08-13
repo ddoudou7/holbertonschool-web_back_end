456741360bf01227a109d6ef6afe4741b98a1dee
MrPhilips <ddoudou7@yahoo.fr>
2025-08-13 20:24:23 +0200

    Task 1: simple pagination

---
+++ pagination/1-simple_pagination.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+"""Simple pagination over the Popular_Baby_Names CSV."""
+
+import csv
+from typing import List
+
+# Import helper from task 0
+index_range = __import__('0-simple_helper_function').index_range
+
+
+class Server:
+    """Server class to paginate a database of popular baby names."""
+
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset"""
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """Return the appropriate page of the dataset (empty list if OOR)."""
+        assert isinstance(page, int) and page > 0
+        assert isinstance(page_size, int) and page_size > 0
+        start, end = index_range(page, page_size)
+        data = self.dataset()
+        return data[start:end]
