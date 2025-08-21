c9da4923c53fa1cbbc470b7fe0a8c8511ddb4e5c
MrPhilips <ddoudou7@yahoo.fr>
2025-08-21 11:16:26 +0200

    ES6_basic: Task 3 Parameter defaults

---
+++ ES6_basic/3-main.js
@@ -0,0 +1,5 @@
+import getSumOfHoods from './3-default-parameter.js';
+
+console.log(getSumOfHoods(34));       // 142
+console.log(getSumOfHoods(34, 3));    // 56
+console.log(getSumOfHoods(34, 3, 4)); // 41
