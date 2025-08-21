83fc4d0e489d57e8c19a292b3a2a32a536fc7b0e
MrPhilips <ddoudou7@yahoo.fr>
2025-08-21 11:02:29 +0200

    ES6_basic: Task 0 + configs + README

---
+++ ES6_basic/.eslintrc.js
@@ -0,0 +1,6 @@
+module.exports = {
+  env: { browser: false, es2021: true, jest: true },
+  extends: ['airbnb-base'],
+  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+  rules: {},
+};
---
+++ ES6_basic/README.md
@@ -0,0 +1,11 @@
+# ES6_basic
+
+Exercises for ES6 basics (Holberton).
+- Task 0: Const or let
+- Tested with Jest, transpiled with Babel, linted with ESLint.
+
+## Run
+```bash
+node 0-main.js
+```
+
