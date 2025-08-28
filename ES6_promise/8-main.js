98b98c197279d9c7a84b1e7bcdea59406acc314c
MrPhilips <ddoudou7@yahoo.fr>
2025-08-28 03:58:20 +0200

    8. Throw an error

---
+++ ES6_promise/8-main.js
@@ -0,0 +1,9 @@
+import divideFunction from './8-try.js';
+
+console.log(divideFunction(10, 2));
+
+try {
+  console.log(divideFunction(10, 0));
+} catch (err) {
+  console.log(`Error: ${err.message}`);
+}
