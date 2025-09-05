ee4d24ffa40413b454bd471fdfba5e5c98805b1a
Mr Philips <ddoudou7@yahoo.fr>
2025-09-05 04:56:07 +0200

    Task 8: Organize a complex HTTP server using Express

--- Node_JS_basic/full_server/controllers/StudentsController.js
+++ Node_JS_basic/full_server/controllers/StudentsController.js
@@ -2,44 +2,43 @@ const readDatabase = require('../utils');
 
 class StudentsController {
   static async getAllStudents(req, res) {
-    res.set('Content-Type', 'text/plain');
     const dbPath = process.argv[2];
 
     try {
-      const map = await readDatabase(dbPath);
+      const fieldsMap = await readDatabase(dbPath); // { CS: [...], SWE: [...] }
+      const order = ['CS', 'SWE'];
 
-      // Construit le rapport au format attendu (comme la task 3)
-      const fields = Object.keys(map).sort(); // alpha: CS puis SWE
-      const total = fields.reduce((acc, k) => acc + map[k].length, 0);
+      // total = somme des longueurs
+      const total = order.reduce((acc, k) => acc + (fieldsMap[k]?.length || 0), 0);
 
-      let report = `Number of students: ${total}`;
-      for (const field of fields) {
-        const list = map[field].join(', ');
-        report += `\nNumber of students in ${field}: ${map[field].length}. List: ${list}`;
+      let report = 'This is the list of our students';
+      report += `\nNumber of students: ${total}`;
+      for (const field of order) {
+        const list = fieldsMap[field] || [];
+        report += `\nNumber of students in ${field}: ${list.length}. List: ${list.join(', ')}`;
       }
 
-      res.status(200).send(`This is the list of our students\n${report}`);
+      res.status(200).set('Content-Type', 'text/plain').send(report);
     } catch (err) {
-      res.status(500).send('This is the list of our students\nCannot load the database');
+      res.status(500).set('Content-Type', 'text/plain').send('Cannot load the database');
     }
   }
 
   static async getAllStudentsByMajor(req, res) {
-    res.set('Content-Type', 'text/plain');
     const dbPath = process.argv[2];
     const { major } = req.params;
 
     if (major !== 'CS' && major !== 'SWE') {
-      res.status(500).send('Major parameter must be CS or SWE');
+      res.status(500).set('Content-Type', 'text/plain').send('Major parameter must be CS or SWE');
       return;
     }
 
     try {
-      const map = await readDatabase(dbPath);
-      const list = (map[major] || []).join(', ');
-      res.status(200).send(`List: ${list}`);
+      const fieldsMap = await readDatabase(dbPath);
+      const list = fieldsMap[major] || [];
+      res.status(200).set('Content-Type', 'text/plain').send(`List: ${list.join(', ')}`);
     } catch (err) {
-      res.status(500).send('Cannot load the database');
+      res.status(500).set('Content-Type', 'text/plain').send('Cannot load the database');
     }
   }
 }
