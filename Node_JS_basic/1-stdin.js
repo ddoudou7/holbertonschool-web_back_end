6cd3df60fbfa0e4bc18e008c0d96df7506c0b30b
Mr Philips <ddoudou7@yahoo.fr>
2025-09-05 03:24:07 +0200

    Task 1: Using Process stdin

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,14 +1,12 @@
-console.log('Welcome to Holberton School, what is your name?');
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.setEncoding('utf8');
 
-process.stdin.on('data', (chunk) => {
-  const name = chunk.trim();
-  if (name) {
-    console.log(`Your name is: ${name}`);
-  }
+process.stdin.on('data', (data) => {
+  const name = data.toString().trim();
+  process.stdout.write(`Your name is: ${name}\n`);
 });
 
 process.stdin.on('end', () => {
-  console.log('This important software is now closing');
+  process.stdout.write('This important software is now closing\n');
 });
