db542a33029ea1193f7b2f77a43eeb601da62f6a
Mr Philips <ddoudou7@yahoo.fr>
2025-09-05 03:21:22 +0200

    Task 1: Using Process stdin

---
+++ Node_JS_basic/1-stdin.js
@@ -0,0 +1,14 @@
+console.log('Welcome to Holberton School, what is your name?');
+
+process.stdin.setEncoding('utf8');
+
+process.stdin.on('data', (chunk) => {
+  const name = chunk.trim();
+  if (name) {
+    console.log(`Your name is: ${name}`);
+  }
+});
+
+process.stdin.on('end', () => {
+  console.log('This important software is now closing');
+});
